2836e639add0d8b2bdf95765e13445a364b8cc7e
daitomiun <mateo.rodriguez.fierro@gmail.com>
2025-12-30 11:54:11 -0500

    Database connector created, new queries added

--- handlers.go
+++ handlers.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
+
+	"github.com/daitonium/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) handlerResetMetrics(w http.ResponseWriter, r *http.Request) {
 	r.Header.Add("charset", "utf-8")
 	r.Header.Add("Content-Type", "text/plain")
+
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Cannot access resource", nil)
+		return
+	}
+
+	err := cfg.database.DeleteUsers(context.Background())
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Something happened while trying to reset users", err)
+		return
+	}
+
 	cfg.fileserverHits.Swap(0)
 	fmt.Printf("Counter reset to %v \n", cfg.fileserverHits.Load())
 	w.WriteHeader(200)
@@ -38,11 +57,8 @@ func handlerCheckHealth(w http.ResponseWriter, r *http.Request) {
 }
 
 func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-	type errorRes struct {
-		Error string `json:"error"`
-	}
 	type successRes struct {
-		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
 	}
 
 	type parameters struct {
@@ -59,7 +75,8 @@ func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
 		respondWithError(w, http.StatusBadRequest, "Chirpy is too long", nil)
 		return
 	}
-	respondWithJSON(w, http.StatusOK, successRes{Valid: true})
+
+	respondWithJSON(w, http.StatusOK, successRes{CleanedBody: replaceBadWords(chirp.Data)})
 }
 
 func respondWithError(w http.ResponseWriter, code int, msg string, err error) {
@@ -85,4 +102,51 @@ func respondWithJSON(w http.ResponseWriter, code int, payload any) {
 	}
 	w.WriteHeader(code)
 	w.Write(dat)
+}
+
+func replaceBadWords(sentence string) string {
+	words := strings.Split(sentence, " ")
+
+	badWords := [3]string{"kerfuffle", "sharbert", "fornax"}
+
+	for i, word := range words {
+
+		for _, badWord := range badWords {
+			lower := strings.ToLower(word)
+			if lower == badWord {
+				words[i] = "****"
+			}
+		}
+
+	}
+	newSentence := strings.Join(words, " ")
+	return newSentence
+}
+
+func (apiCfg *apiConfig) handlerUsers(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Email string `json:"email"`
+	}
+	params := parameters{}
+	decoder := json.NewDecoder(r.Body)
+
+	if err := decoder.Decode(&params); err != nil {
+		log.Printf("Error decoding chirp: %s  \n", err)
+		respondWithError(w, http.StatusInternalServerError, "Could not decode params", err)
+		return
+	}
+
+	user, err := apiCfg.database.CreateUser(context.Background(), params.Email)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Cannot insert user", nil)
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
+
 }
--- main.go
+++ main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+	"time"
+
+	"github.com/daitonium/chirpy/internal/database"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
 func main() {
+
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("Url must be set")
+	}
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error while trying to connect to database: %v", err)
+	}
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		database:       database.New(db),
+		platform:       os.Getenv("PLATFORM"),
 	}
 
 	mux := http.NewServeMux()
@@ -21,6 +49,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerResetMetrics)
 	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsers)
 
 	port := "8080"
 
@@ -28,7 +57,9 @@ func main() {
 		Handler: mux,
 		Addr:    ":" + port,
 	}
-
+	if err != nil {
+		log.Fatal(err)
+	}
 	log.Printf("Listening to port: %s", port)
 
 	log.Fatal(server.ListenAndServe())
